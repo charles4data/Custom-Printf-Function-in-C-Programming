2456a791635b9ae97e0ccd48cce8e47db67dc849
Julius <cjmatheka@gmail.com>
2023-10-21 17:38:23 +0300

    update the custom printf function

--- f_printf.c
+++ f_printf.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdarg.h>
 
 /**
 * _printf - prints formatted strings,
@@ -8,5 +9,37 @@
 
 int _printf(char *format, ...)
 {
+    var_list args;
+    var_start(args, format);
 
+    while (*format)
+    {
+        if (*format == '%')
+        {
+            format++;
+            {
+                if (*format == '%')
+                {
+                    putchar('%');
+                    counter++;
+                }
+                else if (*format == 'c')
+                {
+                    int ch = va_arg(args, int);
+                    putchar(ch);
+                    counter++;
+                }
+                else if (*format == 's')
+                {
+                    char *str = va_arg(args, char*);
+                    while (*str)
+                    {
+                        putchar(*str);
+                        *str++;
+                        counter++;
+                    }
+                }
+            }
+        }
+    }
 }
